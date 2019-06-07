f2f83d0b673f5844637a290788e4ea196ca117e2
Eugene <32429730+J-win@users.noreply.github.com>
2019-06-07 13:51:30 +0300

    add raytracing

---
+++ CG_raytracing/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CG_raytracing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.glControl1 = new OpenTK.GLControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(13, 13);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(774, 424);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.openGlControl_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.openGlControl_Paint);
+            this.glControl1.Resize += new System.EventHandler(this.openGlControl_Resize);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CG_raytracing/Material.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_raytracing
+{
+    class Material
+    {
+        public Vector3 Color;
+        public Vector4 LightCoeffs;
+        public float ReflectionCoef;
+        public float RefractionCoef;
+        public int MaterialType;
+        public Material(Vector3 color, Vector4 lightCoefs, float reflectionCoef, float refractionCoef, int type)
+        {
+            Color = color;
+            LightCoeffs = lightCoefs;
+            ReflectionCoef = reflectionCoef;
+            RefractionCoef = refractionCoef;
+            MaterialType = type;
+        }
+    }
+}
